58a930aa5a20185ce89f706708f40328e0ce5e62
Thiago Oliveira <88551879+thiagopdo@users.noreply.github.com>
2026-03-03 11:45:19 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const port = 8000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("Hello from the server!");
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
